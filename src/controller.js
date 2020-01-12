1c162830050bfb09645cc33c9ec4b73045f61a3e
Mira <calopter@gmail.com>
2020-01-12 13:51:54 -0800

    remove routing logic from controller

--- src/controller.js
+++ src/controller.js
@@ -12,8 +12,6 @@ const respondForm = (url, content) => {
 }
 
 const write = async request => {
-  if (!(request.method === 'POST')) throw(request)
-
   try {
     const title = new URL(request.url).pathname
     const data = await request.formData()
@@ -21,20 +19,18 @@ const write = async request => {
 
     return Response.redirect(title)
   }
-  catch (err) { throw(request) }
+  catch (err) { console.log('failed to write page', err) }
 }
 
 const newForm = async request => {
-  const url = new URL(request.url)
-  if (!url.searchParams.get('edit')) throw(request)
-
   try {
+    const url = new URL(request.url)
     const content = await dbRead(url.pathname)
     
     if (content) return respondForm(url, content)
     return respondForm(url, `# ${url.pathname.slice(1)}\n\n***`)
   }
-  catch (err) { throw(request) }
+  catch (err) { console.log('failed to generate new page form', err) }
 }
 
 const readCache = async request => {
@@ -43,7 +39,7 @@ const readCache = async request => {
     if (!response) throw(request)
     return response
   }
-  catch (err) { throw(request) }
+  catch (err) { throw(err) }
 }
 
 const readDb = async request => {
@@ -53,7 +49,7 @@ const readDb = async request => {
     const content = await dbGet(url.pathname, request)
     return respondMD(url, content)
   }
-  catch (err) { throw(request) }
+  catch (err) { throw(err) }
 }
 
 const create = ({ url }) => Response.redirect(url + '?edit=true')
