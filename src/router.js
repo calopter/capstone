41e1125b4ab38efb7c795e7936c1e6f00048817f
Mira <calopter@gmail.com>
2020-01-12 13:44:35 -0800

    implement routing

--- src/router.js
+++ src/router.js
@@ -1,11 +1,17 @@
 const { write, newForm, readOrCreate } = require('./controller')
 
+const routes = [
+  [ ({ method }) => method === 'POST', write ],
+  [ ({ url }) => new URL(url).searchParams.get('edit'), newForm ],  [ (_) => true, readOrCreate],
+]
+
 const route = e => {
-  e.respondWith(
-    write(e.request)
-      .catch(newForm)
-      .catch(readOrCreate)//.catch(fetch)
-  )
+  for (const [pred, action] of routes) {
+    if (pred(e.request)) {
+      e.respondWith(action(e.request))
+      break
+    }
+  }
 }
 
 module.exports = route
