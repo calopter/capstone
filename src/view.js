2742cfe1a350ef96f8a7ee9b69753eaedd509520
Mira <calopter@gmail.com>
2020-01-12 18:34:16 -0800

    a bit of styling for upload

--- src/view.js
+++ src/view.js
@@ -33,16 +33,25 @@ const form = (url, contents) => {
       </head>
       <body class="bg-washed-green">
         <nav class="flex justify-between bg-light-green mid-gray mw-100">
-          <a class="link dim mid-gray f6 f5-ns dib mv4 ml3"
+        <div>
+          <a class="link dim mid-gray f6 f5-ns dib mt2 mb1 ml3"
               href="/index">index</a>
-          <p class="f6 mv4">
+          <p class="f6 mt1 mb2 ml3">
             editing: <a class="link" href=${url.pathname}>
               ${url.pathname.slice(1)}
             </a>
           </p>
+          </div>
           <div class="flex flex-column justify-around w-25 mr3 min-h-100">
+            <label class="f6 h-50 border-box grow br-pill ba b--black-20 bw1
+                  h-10 bg-washed-green dim mid-gray">
+              <span class="pl4 pa4">+</span>
+                <input class="h1 db o-0" form="content" type="file" id="file"/>
+              </label>
+            </div>
+            <div class="flex flex-column justify-around w-25 mr3 min-h-100">
             <button class="f6 w-100 border-box grow br-pill ba bw1
-              ph3 pv2 h-50 mr4-ns bg-washed-green dim mid-gray"
+              ph3 pt2 pb3 h-50 mr4-ns bg-washed-green dim mid-gray"
               type="submit"
               form="content"/>
                 submit
@@ -50,10 +59,10 @@ const form = (url, contents) => {
           </div>
         </nav>
         <section>
-          <form class="pt3 black-80" id="content" method="post">
-            <input type="file" id="file"/>
+          <form class="pt1 black-80" id="content" method="post">
             <div class="flex flex-wrap justify-center h-75">
-              <textarea class="bg-washed-green input-reset dib
+
+            <textarea class="bg-washed-green input-reset dib
                 border-box ba b--black-20 pa2 br2 mh2 h-100 w-100"
                 id="body" name="body" autofocus>${contents}</textarea>
             </div>
