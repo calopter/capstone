9e666449a85a87764e2e42073435ec157a89f8e1
Mira <calopter@gmail.com>
2020-01-19 19:25:43 -0800

    show key with index

--- src/views/components/index.js
+++ src/views/components/index.js
@@ -4,6 +4,8 @@ module.exports = props => {
   return html`
     <section class="mid-gray mh3 mv4 f5 lh-copy">
       ${props.links}
+      <label class="ph2">key:</label>
+      <input class="w-100" value=${props.key}/>
     </section>
   `
 }
--- src/views/main.js
+++ src/views/main.js
@@ -5,14 +5,14 @@ const display = require('./components/display')
 const index = require('./components/index')
 
 module.exports = (state, emit) => {
-  const { init, doc, docHtml, links } = state
+  const { init, doc, docHtml, links, key } = state
   const path = state.params.wildcard
   const editing = state.query.edit
   
   return html`
     <body class="bg-washed-green">
       ${ nav({ path, editing }, emit) }
-      ${ state.index ? index({ links }, emit) : '' }
+      ${ state.index ? index({ links, key }, emit) : '' }
       ${ editing ?
         edit({ doc }, emit) :
         display({ docHtml, links }) }
