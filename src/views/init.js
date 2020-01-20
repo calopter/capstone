424ae43a5513cb5df4482e2ccde46bfae6dc9b43
Mira <calopter@gmail.com>
2020-01-19 19:14:34 -0800

    style the entry page

--- src/views/init.js
+++ src/views/init.js
@@ -13,16 +13,26 @@ module.exports = (state, emit) => {
   
   return html`
     <body class="bg-washed-green">
-      <section>
+      <nav class="bg-light-green pv4 h3"></nav>
+      <section class="flex flex-wrap justify-center mid-gray
+        mh-100 mw-90 w-90 pa4 center tc">
+        <h1 class="f1 lh-title mv2">welcome</h1>
+        <h1 class="f2 lh-title">
+          use a key, or enter to create a new wiki
+        </h4>
         <form class="pt1 black-80" id="content" onsubmit=${onSubmit}>
-          <div class="flex flex-wrap justify-center">
-            <label for="key">key:</label>
-            <input
+            <input class="input-reset mid-gray ba b--black-20 pa2
+              mb2 db w-100"
               type="text"
               id="key" name="key"
               value="${state.key}"/>
-          </div>
-          <button type="submit">enter</button>
+              
+          <button class="center f6 w-100 border-box grow br-pill ba bw1
+          ph3 pv2 h-50 mr4-ns bg-washed-green dim mid-gray"
+          type="submit"/>
+            enter
+          </button>          
+
         </form>
       </section>
     </body>
